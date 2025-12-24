95b39385a550a760e7693de2a625ac0485bab960
越川昴 <subaru-koshikawa@kiseki.systems>
2025-12-24 16:44:08 +0900

    fix//ファイル内部の記述の修正 カテゴリ検索結果のテスト、コメントを修正致しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -39,17 +39,13 @@ public class Case05 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
-		// 待機時間の設定（Implicit Wait）
 		webDriver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(5));
 
-		// 遷移
 		goTo("http://localhost:8080/lms");
 
-		// 画面が正しく表示されているか検証（アサーション）
 		String actualTitle = webDriver.getTitle();
 		assertEquals("ログイン | LMS", actualTitle, "ログイン画面が表示されていること");
 
-		// エビデンス取得
 		getEvidence(new Object() {
 		});
 	}
@@ -59,21 +55,17 @@ public class Case05 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
-		// 要素の取得
 		WebElement idInput = webDriver.findElement(By.name("loginId"));
 		WebElement passInput = webDriver.findElement(By.name("password"));
 		WebElement loginBtn = webDriver.findElement(By.cssSelector("input[value='ログイン']"));
 
-		// 操作処理
 		idInput.sendKeys("StudentAA01");
 		passInput.sendKeys("StudentBB01");
 		loginBtn.click();
 
-		// 画面が正しく表示されているか検証（アサーション）
 		String actualTitle = webDriver.getTitle();
 		assertEquals("コース詳細 | LMS", actualTitle, "コース詳細画面が表示されていること");
 
-		// エビデンス取得
 		getEvidence(new Object() {
 		});
 	}
@@ -83,23 +75,17 @@ public class Case05 {
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
 		// TODO ここに追加
-		// 要素の取得
 		WebElement menuToggle = webDriver.findElement(By.className("dropdown-toggle"));
 
-		// 操作処理
 		menuToggle.click();
 
-		// 次の要素を取得
 		WebElement helpLink = webDriver.findElement(By.linkText("ヘルプ"));
 
-		// 操作処理
 		helpLink.click();
 
-		// 画面が正しく表示されているか検証（アサーション）
 		String actualTitle = webDriver.getTitle();
 		assertEquals("ヘルプ | LMS", actualTitle, "ヘルプ画面が表示されていること");
 
-		// エビデンス取得
 		getEvidence(new Object() {
 		});
 	}
@@ -109,21 +95,16 @@ public class Case05 {
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
 		// TODO ここに追加
-		// 要素を取得
 		WebElement faqLink = webDriver.findElement(By.linkText("よくある質問"));
 
-		// 操作処理
 		faqLink.click();
 
-		// 別タブへの遷移
 		java.util.List<String> handles = new java.util.ArrayList<>(webDriver.getWindowHandles());
 		webDriver.switchTo().window(handles.get(1));
 
-		// 画面が正しく表示されているか検証（アサーション）
 		String actualTitle = webDriver.getTitle();
 		assertEquals("よくある質問 | LMS", actualTitle, "よくある質問画面が表示されていること");
 
-		// エビデンス取得
 		getEvidence(new Object() {
 		});
 
@@ -134,25 +115,20 @@ public class Case05 {
 	@DisplayName("テスト05 キーワード検索で該当キーワードを含む検索結果だけ表示")
 	void test05() {
 		// TODO ここに追加;
-		// 要素取得
 		WebElement keywordInput = webDriver.findElement(By.name("keyword"));
 		WebElement searchBtn = webDriver.findElement(By.cssSelector("input[value='検索']"));
 
-		// 操作処理
+
 		keywordInput.sendKeys("助成金");
 		searchBtn.click();
 
-		// 検索結果のリストを取得して確認
 		String KeywordResult = webDriver.findElement(By.name("keyword")).getAttribute("value");
 		assertTrue(KeywordResult.contains("助成金"), "検索結果にキーワード '助成金' が含まれていません");
 
-		// タグ要素の取得
 		java.util.List<WebElement> resultList = webDriver.findElements(By.tagName("td"));
 
-		// 検索結果が1件以上あることの確認
 		assertFalse(resultList.isEmpty(), "検索結果が0件です。キーワードに該当するデータが存在しません。");
 
-		// 全ての（td）の中に「助成金」という文字が含まれているかループで確認
 		for (WebElement result : resultList) {
 			String text = result.getText();
 			if (!text.isEmpty()) {
@@ -160,10 +136,9 @@ public class Case05 {
 			}
 		}
 
-		// 一番下までスクロールする
 		scrollTo("document.body.scrollHeight");
 
-		// エビデンス取得
+
 		getEvidence(new Object() {
 
 		});
@@ -174,20 +149,15 @@ public class Case05 {
 	@DisplayName("テスト06 「クリア」ボタン押下で入力したキーワードを消去")
 	void test06() {
 		// TODO ここに追加
-		// 画面をスクロール
 		scrollTo("0");
 
-		// 要素取得
 		WebElement clearBtn = webDriver.findElement(By.cssSelector("input[value='クリア']"));
 
-		// 操作処理
 		clearBtn.click();
 
-		// 検索結果のリストを取得して確認
 		String KeywordResult = webDriver.findElement(By.name("keyword")).getAttribute("value");
 		assertTrue(KeywordResult.contains(""), "検索結果にキーワード が含まれていません");
 
-		// エビデンス取得
 		getEvidence(new Object() {
 		});
 	}
